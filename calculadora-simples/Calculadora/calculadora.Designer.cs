d7f64cad9f340db6f23c74ca7db0d4821d2592c9
MahFLima <57374423+MahFLima@users.noreply.github.com>
2020-04-17 18:23:53 -0300

    Add files via upload

---
+++ calculadora-simples/Calculadora/calculadora.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Calculadora
+{
+    partial class calculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPValor = new System.Windows.Forms.TextBox();
+            this.txtSValor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.botaoSoma = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPValor
+            // 
+            this.txtPValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPValor.Location = new System.Drawing.Point(12, 130);
+            this.txtPValor.Name = "txtPValor";
+            this.txtPValor.Size = new System.Drawing.Size(130, 50);
+            this.txtPValor.TabIndex = 0;
+            // 
+            // txtSValor
+            // 
+            this.txtSValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSValor.Location = new System.Drawing.Point(12, 224);
+            this.txtSValor.Name = "txtSValor";
+            this.txtSValor.Size = new System.Drawing.Size(130, 50);
+            this.txtSValor.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Primeiro valor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Segundo valor";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDividir);
+            this.groupBox1.Controls.Add(this.btnMulti);
+            this.groupBox1.Controls.Add(this.btnSub);
+            this.groupBox1.Controls.Add(this.botaoSoma);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(168, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 163);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operações";
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(102, 93);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 42);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(21, 93);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(75, 42);
+            this.btnMulti.TabIndex = 2;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(102, 45);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 42);
+            this.btnSub.TabIndex = 1;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // botaoSoma
+            // 
+            this.botaoSoma.Location = new System.Drawing.Point(21, 45);
+            this.botaoSoma.Name = "botaoSoma";
+            this.botaoSoma.Size = new System.Drawing.Size(75, 42);
+            this.botaoSoma.TabIndex = 0;
+            this.botaoSoma.Text = "+";
+            this.botaoSoma.UseVisualStyleBackColor = true;
+            this.botaoSoma.Click += new System.EventHandler(this.botaoSomar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(12, 20);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(353, 64);
+            this.txtResultado.TabIndex = 5;
+            this.txtResultado.Text = "     ";
+            // 
+            // calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 322);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSValor);
+            this.Controls.Add(this.txtPValor);
+            this.Name = "calculadora";
+            this.Text = "Calculadora - C# :: Janynne Gomes";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPValor;
+        private System.Windows.Forms.TextBox txtSValor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label txtResultado;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button botaoSoma;
+    }
+}
+
---
+++ calculadora-simples/Calculadora/calculadora.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class calculadora : Form
+    {
+        private double primeiroValor;
+        private double segundoValor;
+        private double Resultado;
+
+
+        
+        public calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void botaoSomar_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtPValor.Text) || string.IsNullOrWhiteSpace(txtSValor.Text))
+            {
+                MessageBox.Show("Informe os Valores");
+                return;
+            }
+
+            String X = " ";
+                primeiroValor = Convert.ToDouble(txtPValor.Text);
+                segundoValor = Convert.ToDouble(txtSValor.Text);
+                Resultado = primeiroValor + segundoValor;
+
+                X = Convert.ToString(Resultado);
+                txtResultado.Text = X;
+
+                LimparValores();
+            
+        }
+
+        protected void LimparValores()
+        {
+            txtPValor.Clear();
+            txtSValor.Clear();
+
+            txtPValor.Focus();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtPValor.Text) || string.IsNullOrWhiteSpace(txtSValor.Text))
+            {
+                MessageBox.Show("Informe os Valores");
+                return;
+            }
+
+            String X = " ";
+            primeiroValor = Convert.ToDouble(txtPValor.Text);
+            segundoValor = Convert.ToDouble(txtSValor.Text);
+            Resultado = primeiroValor - segundoValor;
+
+            X = Convert.ToString(Resultado);
+            txtResultado.Text = X;
+
+            LimparValores();
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtPValor.Text) || string.IsNullOrWhiteSpace(txtSValor.Text))
+            {
+                MessageBox.Show("Informe os Valores");
+                return;
+            }
+
+            String X = " ";
+            primeiroValor = Convert.ToDouble(txtPValor.Text);
+            segundoValor = Convert.ToDouble(txtSValor.Text);
+            Resultado = primeiroValor * segundoValor;
+
+            X = Convert.ToString(Resultado);
+            txtResultado.Text = X;
+
+            LimparValores();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtPValor.Text) || string.IsNullOrWhiteSpace(txtSValor.Text))
+            {
+                MessageBox.Show("Informe os Valores");
+                return;
+            }
+
+            String X = " ";
+            primeiroValor = Convert.ToDouble(txtPValor.Text);
+            segundoValor = Convert.ToDouble(txtSValor.Text);
+            Resultado = primeiroValor / segundoValor;
+
+            X = Convert.ToString(Resultado);
+            txtResultado.Text = X;
+
+            LimparValores();
+        }
+    }
+}
